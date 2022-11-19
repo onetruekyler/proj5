3dd994a7113e443bd7a5ef372610505a83424fbc
Kyler Rasnick <kylerrasnick@live.com>
2022-11-19 18:07:16 -0500

    finished project

--- handlesbars.js
+++ handlesbars.js
@@ -1,17 +1,15 @@
 async function getData(selected_major) {
     var response = await fetch('cit5students.json');
-
     if(response.ok) {
-        var data = await response.json();     // variable named data receives an array of objects
-
-        major_items = data.filter( (item) => item.major == selected_major );    // filter data array for selected meal items 
-
-        var templateText = document.getElementById('tableTemplate').innerHTML;  // Get template text 
-        var compiledTemplateText = Handlebars.compile(templateText);            // Compile the Handlebars template
-        compiledHtml = compiledTemplateText({ rows: major_items })                     // Evaluate the template
-        document.getElementById('majorsTable').innerHTML = compiledHtml; 	   // put html table into division
+        var data = await response.json();     
+        major_items = data.filter( (item) => item.major == selected_major );   
+        var templateText = document.getElementById('tableTemplate').innerHTML;
+        var compiledTemplateText = Handlebars.compile(templateText); 
+        compiledHtml = compiledTemplateText({ rows: major_items })                    
+        document.getElementById('majorsTable').innerHTML = compiledHtml; 	 
   }
   else {
       document.querySelector('#majorsTable').innerHTML = "Data not found";
   }
 }
+
